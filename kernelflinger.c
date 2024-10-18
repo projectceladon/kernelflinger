befde2e5f9f2b9c19957f01485dd8af0edd121e8
Gang G Chen <gang.g.chen@intel.com>
2024-10-18 09:05:46 +0800

    [Test only] Don't set device as unlock state by default on userdebug build
    Signed-off-by: Gang G Chen <gang.g.chen@intel.com>

--- kernelflinger.c
+++ kernelflinger.c
@@ -1618,7 +1618,7 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table)
 #ifndef USER
 	/* WA patch to set device as unlocked by default for userdebug build
 	 */
-	set_current_state(UNLOCKED);
+//	set_current_state(UNLOCKED);
 #else
 	/* For civ, flash images to disk is not MUST. So set device to LOCKED
 	 * state by default on the first boot.
