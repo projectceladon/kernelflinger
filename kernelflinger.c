19c2aba6cc02d74190fea63a06730ea0af4c74d6
Gang G Chen <gang.g.chen@intel.com>
2024-11-14 22:08:40 +0800

    Remove set device as unlocked state in userdebug build
    how to lock/unlock device:
    adb reboot bootloader
    fastboot flashing lock
    fastboot flahsing unlock
    
    Test Done:
    Boot
    
    ori-Tracked-On: OAM-126606
    Tracked-On: OAM-127436
    Signed-off-by: Gang G Chen <gang.g.chen@intel.com>

--- kernelflinger.c
+++ kernelflinger.c
@@ -1496,7 +1496,9 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table)
 	VOID *bootimage = NULL;
 	BOOLEAN oneshot = FALSE;
 	BOOLEAN lock_prompted = FALSE;
+#ifndef USE_SBL
 	BOOLEAN need_lock;
+#endif
 	enum boot_target boot_target = NORMAL_BOOT;
 	UINT8 boot_state = BOOT_STATE_GREEN;
 	VBDATA *vb_data = NULL;
@@ -1611,6 +1613,7 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table)
 		}
 	}
 
+#ifndef USE_SBL
 	need_lock = device_need_locked();
 
 #ifndef USER
@@ -1623,6 +1626,7 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table)
 	*/
 	if (need_lock)
 		set_current_state(LOCKED);
+#endif
 #endif
 
 	if (boot_target == POWER_OFF)
